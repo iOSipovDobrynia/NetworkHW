4cca4b77e9d2c36bcb5a5aee604b99d8d9a387cb
iOSipovDobrynia <dobrynia.osipov@gmail.com>
2023-09-09 13:03:28 +0300

    setup project2.0

---
+++ NetworkHW/Services/NetworkManager.swift
@@ -0,0 +1,12 @@
+//
+//  NetworkManager.swift
+//  NetworkHW
+//
+//  Created by Goodwasp on 08.09.2023.
+//
+
+import Foundation
+
+enum Link: String {
+    case users = "https://randomuser.me/api/?results=15"
+}
--- NetworkHW/ViewControllers/MainViewController.swift
+++ NetworkHW/ViewControllers/MainViewController.swift
@@ -7,91 +7,26 @@
 
 import UIKit
 
-enum Link: String {
-    case user = "https://randomuser.me/api/"
-    case users = "https://randomuser.me/api/?results=5"
-    case female = "https://randomuser.me/api/?gender=female"
-    case male = "https://randomuser.me/api/?gender=male"
-}
-
-enum Action: String, CaseIterable {
-    case fetchUser = "Fetch User"
-    case fetchUsers = "Fetch Users"
-    case fetchFemale = "Fetch Female"
-    case fetchMale = "Fetch Male"
-}
-
 final class MainViewController: UICollectionViewController {
-
-    // MARK: - Private properties
-    private let actions = Action.allCases
-    
+   
     // MARK: - UICollectionViewDataSource
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        actions.count
+        return 0
     }
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "action", for: indexPath)
-        guard let actionCell = cell as? ActionCell else { return UICollectionViewCell() }
-        actionCell.actionLabel.text = actions[indexPath.item].rawValue
         
         return cell
     }
     
     // MARK: - UICollectionViewDelegate
     override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        let action = actions[indexPath.item]
-        switch action {
-        case .fetchUser:
-            fetchUser()
-        case .fetchUsers:
-            fetchUsers()
-        case .fetchFemale:
-            fetchFemale()
-        case .fetchMale:
-            fetchMale()
-        }
+
     }
     
     // MARK: - Private func
-    private func fetchUser() {
-        guard let url = URL(string: Link.user.rawValue) else { return }
-        fetchData(fromURL: url)
-    }
-    
     private func fetchUsers() {
-        guard let url = URL(string: Link.users.rawValue) else { return }
-        fetchData(fromURL: url)
-    }
-    
-    private func fetchFemale() {
-        guard let url = URL(string: Link.female.rawValue) else { return }
-        fetchData(fromURL: url)
-    }
-    
-    private func fetchMale() {
-        guard let url = URL(string: Link.male.rawValue) else { return }
-        fetchData(fromURL: url)
-    }
-    
-    private func fetchData(fromURL url: URL) {
-        URLSession.shared.dataTask(with: url) { data, _, error in
-            guard let data = data else {
-                print(error?.localizedDescription ?? "There is no localized description")
-                return
-            }
-            
-            let decoder = JSONDecoder()
-            
-            do {
-                let result = try decoder.decode(Result.self, from: data)
-                print(result)
-            } catch let error {
-                print(error.localizedDescription)
-                print(error)
-            }
-        }.resume()
     }
 }
 
